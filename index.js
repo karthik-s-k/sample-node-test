fa7fbf8691611c91793be7e4f1df281ab117f26e
Karthik SK <41192803+karthiksk-ef@users.noreply.github.com>
2021-01-18 19:08:55 +0530

    Update port to 3061

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ app.get('/', function (req, res) {
   res.send('Hello World!');
 });
 
-app.listen(3050, function () {
-  console.log('Sample app listening on port 3050!');
+app.listen(3061, function () {
+  console.log('Sample app listening on port 3061!');
 });
